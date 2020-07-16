3a743a1609b7d135a90445abaa3d04cf6fba6737
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-07-16 13:45:43 +0300

    fix rubocop assert

--- lib/codebreaker_console/difficulty_state.rb
+++ lib/codebreaker_console/difficulty_state.rb
@@ -3,7 +3,7 @@
 module CodebreakerConsole
   class DifficultyState < GameState
     def execute
-      puts I18n.t(:difficulty_message, difficulties: difficulties.join(', '))
+      puts I18n.t(:difficulty_message, difficulties: difficulties)
 
       context.game = Codebreaker::GameFactory.new.create_game(context.user, user_input.to_sym)
       context.transit_to(PlayState.new)
@@ -16,7 +16,7 @@ module CodebreakerConsole
         Codebreaker::GameFactory::EASY_DIFFICULTY.name,
         Codebreaker::GameFactory::MEDIUM_DIFFICULTY.name,
         Codebreaker::GameFactory::HELL_DIFFICULTY.name
-      ]
+      ].join(', ')
     end
 
     private
